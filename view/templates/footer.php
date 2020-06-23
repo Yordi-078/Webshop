5b09b33993526c982ba22df23b55a056cbb92972
timo lemmen <99058056@mydavinci.nl>
2020-06-23 12:04:32 +0200

    layout

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -1,4 +1,33 @@
+<footer class="container-fluid padding">
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <img src="~/img/logo.png" width="50" height="50">
+                    <hr class="light">
+                    <p>06123456</p>
+                    <p><a href="mailto:info@marimowebsites.nl">info@marimowebsites.nl</a></p>
+                </div>
 
+                <div class="col-md-4">
+                    <hr class="light">
+                    <h5>onze uren</h5>
+                    <hr class="light">
+                    <p>maandag: 08:00 - 17:00</p>
+                    <p>zaterdag: 10:00 - 15:00</p>
+                    <p>zondag: Gesloten</p>
+                </div>
+
+                <div class="col-md-3">
+                    <hr class="light">
+                    <h5>Land</h5>
+                    <hr class="light">
+                    <p>Nederland</p>
+                </div>
+                <div class="col-12">
+                    <hr class="light">
+                    <h5>&copy; marimowebsites.nl</h5>
+                </div>
+            </div>
+        </footer>
 </div><!-- end container div -->
 </body>
 </html>
